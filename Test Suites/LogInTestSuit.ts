4c7db58a0f56548c92af5f811a34c7e03f84db99
Nguyễn Thị Thanh Ngân <ngan.nguyen@f4.intek.edu.vn>
2020-09-26 18:04:50 +0700

    Finished Lab01

---
+++ Test Suites/LogInTestSuit.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LogInTestSuit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e4a91fb2-6ab7-4dbe-b606-0801616d738e</testSuiteGuid>
+   <testCaseLink>
+      <guid>bcebb950-e7ac-4d03-86d3-f001fd2c351d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bea9a5d6-ba95-4816-b436-9e7c964cb380</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc02</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9c826378-70e0-4fb6-af82-4c3fc526aef1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc03</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0bd5a4b9-c83b-4f43-9ef8-242dd1bb52ea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc04</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c377f95d-b8a3-49dc-8672-b8099277b7ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc05</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
